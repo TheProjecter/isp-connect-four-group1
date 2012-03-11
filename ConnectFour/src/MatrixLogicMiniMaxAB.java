06ab756c98cafbb24bf4222486ff4ccfe2ad81fc
johanvts <johanvts@gmail.com>
2012-03-11 14:29:31 +0000

    Not quiet working cut-off

--- ConnectFour/src/MatrixLogicMiniMaxAB.java
+++ ConnectFour/src/MatrixLogicMiniMaxAB.java
@@ -2,7 +2,7 @@
 
 public class MatrixLogicMiniMaxAB {
 	int counter =0;
-	int cutoff = 10;
+	int cutoff = 200000;
 	boolean wasCut = false;
 	int winCondition = 4;
 	
@@ -11,22 +11,49 @@ public class MatrixLogicMiniMaxAB {
 
 	int ABsearch(Board board){
 			int v = MaxValue(board,Integer.MIN_VALUE,Integer.MAX_VALUE);
-			if(wasCut) v=0;
+			System.out.println("MaxVal: "+ v);
 			int choice = 0;
 			
 			for (int a: ToolSet.Actions(board)){
 				int b = MinValue(ToolSet.Result(board,a,1),Integer.MIN_VALUE,Integer.MAX_VALUE);
-				if(b==v) {
+					if(wasCut){
+						//Hvis der var et cutoff kan vi ikke regne med at hverken min eller max val er rigtige.
+						//Vi bliver nødt til at spille efter den maksimale af de udregnede minvals.
+						//Hele pointen med cut-off er at vi ikke kan tjekke alle de følgende muligheder. Men vi kan da tjekke
+						//Det næste niveau og sikre os at man ikke spiller et spil der giver modstanderen sejr i næste træk.
+						if(!nextLoose(ToolSet.Result(board,a,1)));{
+							v=ToolSet.Max(v, b);
+							if(b==v) {
+									choice=a;
+							}
+						}
+					}
+					else if(b==v) {
 					choice=a;
 					}
 			}
 			
 			System.out.println("After "+counter+" evaluations.");
 			counter=0;
+			wasCut=false;
 			System.out.println("I have chosen to play: "+choice);
 			System.out.println("For an expected outcome of: "+v);
 			return choice;
 		}
+	
+	/**
+	 * Checks if the opponent can win in the next move on a given board.
+	 * @param board
+	 * @return
+	 */
+	boolean nextLoose(Board board){
+		boolean opWin=false;
+		for (int a1: ToolSet.Actions(board)){
+			State s= new State(ToolSet.Result(board, a1, -1));
+			if(s.getUtility()==-1) opWin=true;
+		}
+		return opWin;
+	}
 	
 	int MaxValue(Board board,int alpha,int beta){
 		counter++;
