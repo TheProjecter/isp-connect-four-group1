1afd664e32b881dcb068389b4d546d7c87f4634c
laugeg@gmail.com <laugeg@gmail.com@0d430875-7949-e57c-1323-bab404a28672>
2012-03-15 15:11:59 +0000

    what the fuck

--- ConnectFour/src/State.java
+++ ConnectFour/src/State.java
@@ -1,11 +1,17 @@
-public class State {
-	private byte utility = 0;
-	private byte winCondition = 4;
-	private byte jHur=0;
 
+public class State {
+	private int utility = 0;
+	private int winCondition;
+	private boolean isFull = false;
+	private int playerWin =  1000000;
+	
 	public State(Board board){
-		utility = utility(board);
-		jHur = jHur(board);
+		this.winCondition = board.getWinCondition();
+		if(board.isFull()){
+			this.isFull = true;
+		}
+		this.utility = setUtility(board);
+		if(utility==99) utility = this.calcBoardHeuristic(board);
 	}
 	
 	public State(int util){
@@ -14,34 +20,30 @@ public class State {
 
 	/**
 	 * Returns the winner of the board.
-	 * The value is multiplied with 10000 to dominate the jHurs value.
 	 * @param board
 	 * @return
 	 */
-	private byte utility(Board board){
-		int huroffset =  10000;
+	private int setUtility(Board board){
     	for(int col=0;col<board.colCount();col++){
-    		byte colWin = (byte) ( calcWinner(board.getColumn(col))*huroffset);
+    		int colWin = calcWinner(board.getColumn(col));
     		if(colWin!=0) return colWin;
     	}
     	
     	for(int row=0;row<board.rowCount();row++){
-    		byte rowWin = (byte) (calcWinner(board.getRow(row))*huroffset);
+    		int rowWin = calcWinner(board.getRow(row));
     		if(rowWin!=0) return rowWin;
     	}
     	
     	for(int lDia=0;lDia<board.diagonalCount();lDia++){
-    		byte lDiaWin = (byte) (calcWinner(board.getLeftDiagonal(lDia))*huroffset);
+    		int lDiaWin = calcWinner(board.getLeftDiagonal(lDia));
     		if(lDiaWin!=0) return lDiaWin;
     	}
     	
     	for(int rDia=0;rDia<board.diagonalCount();rDia++){
-    		byte rDiaWin = (byte) (calcWinner(board.getRightDiagonal(rDia))*huroffset);
+    		int rDiaWin = calcWinner(board.getRightDiagonal(rDia));
     		if(rDiaWin!=0) return rDiaWin;
     	}
-		
-    	if(board.isFull()) return 0;
-    	
+		    	
 		return 99;
 	}
 	
@@ -50,23 +52,26 @@ public class State {
 	 * @return
 	 */
 	public boolean isTerminal(){
-		return utility != 99;
+		return (utility==playerWin || utility == -playerWin || this.isFull);
 	}
 	
 	/**
 	 * Wrapper for the utility function.
 	 * @return
 	 */
-	public byte getUtility(){
+	public int getUtility(){
 		return utility;
 	}
 	
+	public int getPlayerWin(){
+		return playerWin;
+	}
 	/**
 	 * Checks a given Coin array for a winning combination.
 	 * @param coinArr
 	 * @return
 	 */
-	private byte calcWinner(Coin[] coinArr){
+	private int calcWinner(Coin[] coinArr){
 		int oneSum=0;
 		int twoSum=0;
 		
@@ -78,90 +83,121 @@ public class State {
 			else if(coin.isPlayerOne()){
 				oneSum++;
 				twoSum=0;
-				if(oneSum==winCondition) return (byte) 1;
+				if(oneSum == winCondition) return playerWin;
 				}
 			
 			else if(coin.isPlayerTwo()){
 				oneSum=0;
 				twoSum++;
-				if(twoSum==(winCondition)) return (byte) -1;
+				if(twoSum==(winCondition)) return -playerWin;
 				}
 		}
 	
-		return (byte) 0;
+		return 0;
 }
+
+	private int calcBoardHeuristic(Board b){
+		int bHeuristic = 0;
+    	for(int x=0;x<b.colCount();x++){
+    		Coin[] colX = b.getColumn(x);
+    		bHeuristic += this.calcCoinHeuristic(colX);
+    	}
+    	
+    	for(int y=0;y<b.rowCount();y++){
+    		Coin[] rowY = b.getRow(y);
+    		bHeuristic += this.calcCoinHeuristic(rowY);
+    	}
+    	
+    	for(int l=0;l<b.diagonalCount();l++){
+    		Coin[] diaL= b.getLeftDiagonal(l);
+    		bHeuristic += this.calcCoinHeuristic(diaL);
+    	}
+    	
+    	for(int r=0;r<b.diagonalCount();r++){
+    		Coin[] diaR = b.getRightDiagonal(r);
+    		bHeuristic += this.calcCoinHeuristic(diaR);
+    	}
+		
+    	return bHeuristic;
+	}
+	
 	/**
-	 * Checks a given Coin array for it's max-combination.
-	 * This version disregards "closed" arrays. Meaning it doesn't care for coins that are capped by the opponent.
-	 * I have left the "max" parameters to make this easy to adjust by removing the reset lines.
+	 * Calculates the heuristic of a given coin array.
+	 * The function checks if there is a possibility for a winning condition,
+	 * the closer the player is, the higher the power of ten times the player value
+	 * is added to the heuristics.
+	 * E.g. if playerOne has three coins in a row followed by one null value, the added value
+	 * to the heuristic is 1*10^0+1*10^1+1*10^2 = 1+10+100 = 111. 
+	 * Vice versa -1*10^0+-1*10^1+-1*10^2 = -1+-10+-100 = -111 is added to the heuristic.
 	 * @param coinArr
 	 * @return
 	 */
-	public byte calcSum(Coin[] coinArr){
-		int oneSum=0;
-		int twoSum=0;
-		int oneMax=0;
-		int twoMax=0;
+	private int calcCoinHeuristic(Coin[] coinArr){
+		int[] oneHeu = new int[winCondition];
+		int[] twoHeu = new int[winCondition];
+		int oneCount = 0;
+		int twoCount = 0;
+		int heuristic = 0;
 		
 		for (Coin coin:coinArr){
 			if(coin==null){
-				oneSum = 0;
-				twoSum = 0;
+				oneCount++;
+				twoCount++;
 			}
 			else if(coin.isPlayerOne()){
-				oneSum++;
-				twoSum=0;
-				twoMax=0;
-				oneMax=ToolSet.Max(oneSum,oneMax);
+				oneHeu[oneCount] = 1;
+				oneCount++;
+				twoCount=0;
+			}
+			else if(coin.isPlayerTwo()){
+				twoHeu[twoCount] = -1;
+				twoCount++;
+				oneCount=0;
+				}
+			
+			if(oneCount == winCondition){
+				oneCount = 0;
+				int zeroCount = winCondition;
+				int tempHeuristic = 0;
+				
+				for(int one: oneHeu){
+					if(one!=0){
+						int value = (int) Math.pow(10,oneCount);
+						tempHeuristic += value*one;
+						oneCount++;
+						zeroCount--;
+					}
+				}
+				if(zeroCount!=0){
+					heuristic += tempHeuristic;
+				}
+				oneCount = 0;
+				oneHeu = new int[winCondition];
 			}
 			
-			else if(coin.isPlayerTwo()){
-				oneSum=0;
-				oneMax=0;
-				twoSum++;
-				twoMax=ToolSet.Max(twoSum, twoMax);
+			if(twoCount == winCondition){
+				twoCount = 0;
+				int zeroCount = winCondition;
+				int tempHeuristic = 0;
 
+				for(int two: twoHeu){
+					if(two!=0){
+					int value = (int) Math.pow(10,twoCount);
+					heuristic += value*two;
+					twoCount++;
+					zeroCount--;
+					}
+				}
+				if(zeroCount!=0){
+					heuristic += tempHeuristic;
+				}
+				twoCount = 0;
+				twoHeu = new int[winCondition];
 			}
 		}
-		if(oneMax>twoMax) return (byte)oneMax;
-		if(twoMax>oneMax) return (byte)-twoMax;
-		else return (byte) 0;
-}
-	/**
-	 * Finds the jHur of a board.
-	 * jHur is a simple heuristic that returns the number of "friendly" combinations on a board.
-	 * @param board
-	 * @return
-	 */
-	private byte jHur(Board board){
-		int jHur=0;
-    	for(int col=0;col<board.colCount();col++){
-    		jHur += calcSum(board.getColumn(col));
-    	}
-    	
-    	for(int row=0;row<board.rowCount();row++){
-    		jHur += calcSum(board.getRow(row));
-    	}
-    	
-    	for(int lDia=0;lDia<board.diagonalCount();lDia++){
-    		jHur += calcSum(board.getLeftDiagonal(lDia));
-    	}
-    	
-    	for(int rDia=0;rDia<board.diagonalCount();rDia++){
-    		jHur += calcSum(board.getRightDiagonal(rDia));
-    	}
 		
-    	if(board.isFull()) return 0;
-    	
-		return (byte) jHur;
-	}
+		
 	
-	/**
-	 * Returns the jHur value of this state.
-	 * @return
-	 */
-	public byte getjHur(){
-		return jHur;
+		return heuristic;
 	}
-
 }
